b085b4221aeaec90ccad8f2fc52260faf0c17527
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-01-08 14:30:30 +0700

    fixing cookie 4

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -112,11 +112,11 @@ const userLogin = async (req, res) => {
       const { password, _id, activeAccount, ...others } = user._doc;
 
       res.cookie("refresh_token", refreshToken, {
-        httpOnly: false,
+        httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         sameSite: "strict",
         secure: true,
-        path: "https://my-pets-frontend.vercel.app/",
+        path: "/accounts",
       });
       console.log("user login", { user, newToken });
       return res.status(200).json({ ...others, accessToken });
